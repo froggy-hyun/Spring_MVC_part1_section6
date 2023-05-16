a3ee8282553f8180c9fd54eda26749e77ff6c50b
1233day <1233day@naver.com>
2023-05-16 23:06:14 +0900

    46강

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,35 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+/*
+        [올바른 로그 사용법]
+        log.debug("data="+data);
+        로그 출력 레벨을 info로 설정해도 해당 코드에 있는 "data="+data가 실제 실행이 되어 버린다.
+        결과적으로 문자 더하기 연산이 발생한다.
+        log.debug("data={}", data);
+        로그 출력 레벨을 info로 설정하면 아무일도 발생하지 않는다. 따라서 앞과 같은 의미없는 연산이 발생하지 않는다.
+*/
+        return "ok";
+    }
+}
